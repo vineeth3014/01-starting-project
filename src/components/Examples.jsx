824781927d5e18fee28374db1a2af741207b59a1
Vineeth <vineeth.m@altumindtech.com-at-368529449699>
2023-11-07 13:14:55 +0530

    project completed

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-
 import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
+import Tabs from './Tabs'
 import { EXAMPLES } from '../data.js';
+
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -17,7 +19,7 @@ export default function Examples() {
   if (selectedTopic) {
     tabContent = (
       <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <h3>{EXAMPLES[selectedTopic].title}</h3> 
         <p>{EXAMPLES[selectedTopic].description}</p>
         <pre>
           <code>{EXAMPLES[selectedTopic].code}</code>
@@ -27,35 +29,40 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
-      <menu>
-        <TabButton
+    <Section title="Examples" id="examples">
+      <Tabs 
+      // ButtonContainer = "menu" instead decelear it on the destrective parameter
+      buttons = {
+      <>
+      <TabButton
           isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')}
+          onClick={() => handleSelect('components')}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')}
+          onClick={() => handleSelect('jsx')}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')}
+          onClick={() => handleSelect('props')}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')}
+          onClick={() => handleSelect('state')}
         >
           State
         </TabButton>
-      </menu>
+        </>
+       }
+       >
       {tabContent}
-    </section>
+      </Tabs>  
+    </Section>
   );
 }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -4,12 +4,12 @@
 //     className={isSelected ? 'active' : undefined} 
 //     onClick={onSelect}>{children}</button></li>
 // }
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children,  isSelected, ...props }) {
     // console.log('TABBUTTON COMPONENT EXECUTING');
     return (
       <li>
         <button 
-        className={isSelected ? 'active' : undefined} onClick={onSelect}>
+        className={isSelected ? 'active' : undefined} {...props}>
           {children}
         </button>
       </li>
